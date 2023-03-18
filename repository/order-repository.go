41e0f91716ec3135dffe542991530c3ba002d433
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2023-03-18 11:17:38 +0700

    update

--- repository/order-repository.go
+++ repository/order-repository.go
@@ -102,7 +102,7 @@ type SelectedData struct {
 
 func (db *orderRepository) UpdateStock(route_id uint64, product_id uint64, qty float64) {
 	var selectedData SelectedData
-	res := db.connect.Table("order_stock").Select("id,product_id,avl_qty").Where("route_id =?", route_id).Where("product_id = ?", product_id).Where("avl_qty >= ?", qty).Where("order_id = 202653").Scan(&selectedData)
+	res := db.connect.Table("order_stock").Where("route_id =?", route_id).Where("product_id = ?", product_id).Where("avl_qty >= ?", qty).Where("order_id = 202653").Select("id,product_id,avl_qty").Scan(&selectedData)
 	if res.Error == nil {
 		res_update := db.connect.Table("order_stock").Where("id=?", selectedData.Id).Update("avl_qty", (selectedData.AvlQty - qty))
 		if res_update.Error == nil {
@@ -116,7 +116,7 @@ func (db *orderRepository) AddPayment(order_id uint64, customer_id uint64, amoun
 	var findone uint64 = 0
 	current_date := time.Now().Local()
 
-	recid := db.connect.Table("payment_receive").Select("id").Where("customer_id = ?", customer_id).Where("date(trans_date) = ?", current_date.Format("2006-01-02")).Take(&findone)
+	recid := db.connect.Table("payment_receive").Where("customer_id = ?", customer_id).Where("date(trans_date) = ?", current_date.Format("2006-01-02")).Select("id").Take(&findone)
 	if recid != nil {
 		print("not error but not found record")
 		if findone > 0 {
@@ -138,13 +138,16 @@ func (db *orderRepository) AddPayment(order_id uint64, customer_id uint64, amoun
 				CratedBy:   user_id,
 				CreatedAt:  uint64(time.Now().Unix()),
 			}
-			res := db.connect.Table("payment_receive").Create(&payment)
-			if res.Error == nil {
-				res_save_detail := db.connect.Table("payment_receive_line").Create(map[string]interface{}{"payment_receive_id": payment.Id, "order_id": order_id, "payment_amount": amount, "payment_channel_id": 1, "payment_method_id": payment_type_id, "status": 1, "payment_type_id": payment_type_id})
-				if res_save_detail.Error == nil {
-					print("create payment")
+			if payment.JournalNo != "error na ja" {
+				res := db.connect.Table("payment_receive").Create(&payment)
+				if res.Error == nil {
+					res_save_detail := db.connect.Table("payment_receive_line").Create(map[string]interface{}{"payment_receive_id": payment.Id, "order_id": order_id, "payment_amount": amount, "payment_channel_id": 1, "payment_method_id": payment_type_id, "status": 1, "payment_type_id": payment_type_id})
+					if res_save_detail.Error == nil {
+						print("create payment")
+					}
 				}
 			}
+
 		}
 
 	} else {
@@ -152,60 +155,89 @@ func (db *orderRepository) AddPayment(order_id uint64, customer_id uint64, amoun
 	}
 }
 
+type MaxOrderNo struct {
+	Id      uint64 `json:"id"`
+	OrderNo string `json:"order_no"`
+}
+
 func (db *orderRepository) GetLastNo(company_id uint64, branch_id uint64, route_id uint64, route_code string) string {
-	var max_order_no string = ""
+	var max_order_no MaxOrderNo
 	var pre string = "CO-" + route_code
 	var prefix string = ""
 	var cnum string = ""
 	// var cnum2 int64 = 8
 	var cnum3 int64 = 0
+	current_date := time.Now().Local()
 
-	row := db.connect.Table("orders").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("order_channel_id=?", route_id).Where("sale_from_mobile=1").Where("order_no LIKE ?", "CO%").Select("max(order_no)").Row()
-	err := row.Scan(&max_order_no)
-	if err != nil {
-		return "error na ja"
+	// row := db.connect.Table("orders").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("order_channel_id=?", route_id).Where("sale_from_mobile=1").Where("order_no LIKE ?", "CO%").Select("max(order_no)").Row()
+	// err := row.Scan(&max_order_no)
+	// if err != nil {
+	// 	return "error na ja"
+	// }
+	row := db.connect.Table("orders").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("order_channel_id=?", route_id).Where("sale_from_mobile=1").Where("date(order_date)=?", current_date.Format("2006-01-02")).Select("id,order_no").Last(&max_order_no)
+	if row.Error != nil {
+		//return "error na ja"
 	}
 	// if row.Error != nil {
 	// 	print("error na")
 	// }
 
-	//max_order_no = "CO-VP31-230314-0009"
-	var full_year string = strconv.Itoa(time.Now().Year())
-	var full_month string = strconv.Itoa(int(time.Now().Month()))
-	if len(full_month) == 1 {
-		full_month = "0" + full_month
-	}
-	var full_day string = strconv.Itoa(time.Now().Day())
-	if len(full_day) == 1 {
-		full_day = "0" + full_day
-	}
-	prefix = pre + "-" + full_year[2:len(full_year)] + full_month + full_day + "-"
-	cnum = max_order_no[15:len(max_order_no)]
-	// cnum = "000"
-	if cnumx, err := strconv.ParseInt(cnum, 10, 64); err != nil {
-		panic(err)
+	if max_order_no.OrderNo != "" {
+		//max_order_no = "CO-VP31-230314-0009"
+		var full_year string = strconv.Itoa(time.Now().Year())
+		var full_month string = strconv.Itoa(int(time.Now().Month()))
+		if len(full_month) == 1 {
+			full_month = "0" + full_month
+		}
+		var full_day string = strconv.Itoa(time.Now().Day())
+		if len(full_day) == 1 {
+			full_day = "0" + full_day
+		}
+		prefix = pre + "-" + full_year[2:len(full_year)] + full_month + full_day + "-"
+		cnum = max_order_no.OrderNo[15:len(max_order_no.OrderNo)]
+		// cnum = "000"
+		if cnumx, err := strconv.ParseInt(cnum, 10, 64); err != nil {
+			panic(err)
+		} else {
+			//print("okk")
+			cnum3 = cnumx + 1
+			// cnum2 = cnumx
+		}
+		//cnum3 = cnum2 + 1
+
+		var strlen int = len(cnum)
+		var clen int = len(strconv.Itoa(int(cnum3)))
+		var loop int = strlen - clen
+
+		for i := 0; i <= loop-1; i++ {
+			prefix = prefix + "0"
+		}
+		prefix = prefix + strconv.Itoa(int(cnum3))
+
+		//return strconv.Itoa(int(cnum2))
 	} else {
-		//print("okk")
-		cnum3 = cnumx + 1
-		// cnum2 = cnumx
+		var full_year string = strconv.Itoa(time.Now().Year())
+		var full_month string = strconv.Itoa(int(time.Now().Month()))
+		if len(full_month) == 1 {
+			full_month = "0" + full_month
+		}
+		var full_day string = strconv.Itoa(time.Now().Day())
+		if len(full_day) == 1 {
+			full_day = "0" + full_day
+		}
+		prefix = pre + "-" + full_year[2:len(full_year)] + full_month + full_day + "-" + "0001"
 	}
-	//cnum3 = cnum2 + 1
 
-	var strlen int = len(cnum)
-	var clen int = len(strconv.Itoa(int(cnum3)))
-	var loop int = strlen - clen
-
-	for i := 0; i <= loop-1; i++ {
-		prefix = prefix + "0"
-	}
-	prefix = prefix + strconv.Itoa(int(cnum3))
-
-	//return strconv.Itoa(int(cnum2))
 	return prefix
 }
 
+type MaxPayJournalNo struct {
+	Id        uint64 `json:"id"`
+	JournalNo string `json:"journal_no"`
+}
+
 func (db *orderRepository) GetPayLastNo(company_id uint64, branch_id uint64) string {
-	var max_journal_no string = ""
+	var max_journal_no MaxPayJournalNo
 	var pre string = "AR-"
 	var prefix string = ""
 	var cnum string = ""
@@ -214,45 +246,68 @@ func (db *orderRepository) GetPayLastNo(company_id uint64, branch_id uint64) str
 	// var prefix2 strings.Builder
 	current_date := time.Now().Local()
 
-	row := db.connect.Table("payment_receive").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("date(trans_date)=?", current_date.Format("2006-01-02")).Select("max(journal_no)").Row()
-	err := row.Scan(&max_journal_no)
-	if err != nil {
-		return "error na ja"
+	// row := db.connect.Table("payment_receive").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("date(trans_date)=?", current_date.Format("2006-01-02")).Select("max(journal_no)").Row()
+	// err := row.Scan(&max_journal_no)
+	// if err != nil {
+	// 	return "error na ja"
+	// }
+	row := db.connect.Table("payment_receive").Where("company_id=?", company_id).Where("branch_id=?", branch_id).Where("date(trans_date)=?", current_date.Format("2006-01-02")).Select("id,journal_no").Last(&max_journal_no)
+	if row.Error != nil {
+		//panic(row.Error)
+		// if(row.Error.Error() == "record not found"){
+
+		// }
+		// return "error na ja"
 	}
 
-	//max_journal_no = "CO-VP31-230314-0009"
-	var full_year string = strconv.Itoa(time.Now().Year())
-	var full_month string = strconv.Itoa(int(time.Now().Month()))
-	if len(full_month) == 1 {
-		full_month = "0" + full_month
-	}
-	var full_day string = strconv.Itoa(time.Now().Day())
-	if len(full_day) == 1 {
-		full_day = "0" + full_day
-	}
-	//prefix2.WriteString(pre + full_year[2:len(full_year)] + full_month + full_day + "-")
-	prefix = pre + full_year[2:len(full_year)] + full_month + full_day + "-"
-	cnum = max_journal_no[10:len(max_journal_no)]
-	// cnum = "000"
-	if cnumx, err := strconv.ParseInt(cnum, 10, 64); err != nil {
-		panic(err)
+	if max_journal_no.JournalNo != "" {
+		//max_journal_no = "CO-VP31-230314-0009"
+		var full_year string = strconv.Itoa(time.Now().Year())
+		var full_month string = strconv.Itoa(int(time.Now().Month()))
+		if len(full_month) == 1 {
+			full_month = "0" + full_month
+		}
+		var full_day string = strconv.Itoa(time.Now().Day())
+		if len(full_day) == 1 {
+			full_day = "0" + full_day
+		}
+		//prefix2.WriteString(pre + full_year[2:len(full_year)] + full_month + full_day + "-")
+		prefix = pre + full_year[2:len(full_year)] + full_month + full_day + "-"
+		cnum = max_journal_no.JournalNo[10:len(max_journal_no.JournalNo)]
+		// cnum = "000"
+		if cnumx, err := strconv.ParseInt(cnum, 10, 64); err != nil {
+			panic(err)
+		} else {
+			//print("okk")
+			cnum3 = cnumx + 1
+			// cnum2 = cnumx
+		}
+		//cnum3 = cnum2 + 1
+
+		var strlen int = len(cnum)
+		var clen int = len(strconv.Itoa(int(cnum3)))
+		var loop int = strlen - clen
+
+		for i := 0; i <= loop-1; i++ {
+			prefix = prefix + "0"
+		}
+		prefix = prefix + strconv.Itoa(int(cnum3))
+
+		//return strconv.Itoa(int(cnum2))
 	} else {
-		//print("okk")
-		cnum3 = cnumx + 1
-		// cnum2 = cnumx
+		var full_year string = strconv.Itoa(time.Now().Year())
+		var full_month string = strconv.Itoa(int(time.Now().Month()))
+		if len(full_month) == 1 {
+			full_month = "0" + full_month
+		}
+		var full_day string = strconv.Itoa(time.Now().Day())
+		if len(full_day) == 1 {
+			full_day = "0" + full_day
+		}
+		//prefix2.WriteString(pre + full_year[2:len(full_year)] + full_month + full_day + "-")
+		prefix = pre + full_year[2:len(full_year)] + full_month + full_day + "-" + "0001"
 	}
-	//cnum3 = cnum2 + 1
 
-	var strlen int = len(cnum)
-	var clen int = len(strconv.Itoa(int(cnum3)))
-	var loop int = strlen - clen
-
-	for i := 0; i <= loop-1; i++ {
-		prefix = prefix + "0"
-	}
-	prefix = prefix + strconv.Itoa(int(cnum3))
-
-	//return strconv.Itoa(int(cnum2))
 	return prefix
 }
 
